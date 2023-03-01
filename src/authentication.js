fb31e7bfbaf6e362d93e8590235ada56c5150b42
Niels <niels.holvoet@gmail.com>
2023-03-01 18:08:27 +0100

    work in progress

--- src/authentication.js
+++ src/authentication.js
@@ -1,30 +1,75 @@
-const {UserAccountNotFound} = require("./infrastucture/database/userAccounts")
+const { UserAccountNotFound } = require("./infrastucture/database/userAccounts")
+const uuid = require('uuid')
 
-class AuthenticationError extends Error{
+
+class AuthenticationError extends Error {
     constructor(message) {
         super(message);
         this.name = "AuthenticationError";
     }
 }
 
-class UserAuthenticator{
+class Session {
+    constructor(userAccount, expiresAt) {
+        this.userAccount = userAccount
+        this.expiresAt = expiresAt
+    }
 
-    constructor(userAccountRepository){
+    static create(userAccount) {
+        const now = new Date()
+        const expiresAt = new Date(+now + 120 * 1000)
+        return new Session(
+            userAccount,
+            expiresAt
+        )
+    }
+
+    isExpired() {
+        this.expiresAt < new Date()
+    }
+}
+
+class UserAuthenticator {
+
+    constructor(userAccountRepository) {
         this.userAccountRepository = userAccountRepository
+        this.sessions = {}
     }
 
     authenticate = (username, password) => {
-        try{
+        try {
             const userAccount = this.userAccountRepository.findByUsernameAndPassword(username, password)
-            return userAccount
-        }catch(error){
-            if(error instanceof UserAccountNotFound){
+            const session = Session.create(userAccount)
+            const token = uuid.v4()
+            this.sessions[token] = session
+            return { token, session }
+        } catch (error) {
+            if (error instanceof UserAccountNotFound) {
                 throw new AuthenticationError("Invalid credentials")
             }
+            console.log(error)
             throw new Error("unexpected error")
         }
     }
-    
+
+    endSession = (sessionToken) => {
+        delete this.sessions[sessionToken]
+    }
+
+
+    validateSession = (sessionToken) => {
+        const session = this.sessions[sessionToken]
+        if (session) {
+            if (!session.isExpired()) {
+                return session
+            } else {
+                this.endSession(sessionToken)
+            }
+        }
+        return undefined
+
+    }
+
 }
 
-module.exports = {UserAuthenticator, AuthenticationError}
+module.exports = { UserAuthenticator, AuthenticationError }
--- src/infrastucture/web/webServer.js
+++ src/infrastucture/web/webServer.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const { AuthenticationError } = require('../../authentication')
 const TemplateRenderer = require("./templateRenderer")
+const cookieParser = require('cookie-parser')
 
 
 class WebServer {
@@ -17,44 +18,101 @@ class WebServer {
     }
 
     run = () => {
-        const userAuthenticator = this.userAuthenticator
 
         app.use(express.urlencoded({ extended: false }))
+        app.use(cookieParser())
         //routes
-
-        app.get('/', function(req, res){
-            const templateRenderer = TemplateRenderer.createFromFile("index.html")
-            const html =  templateRenderer.renderWithoutData()
-            res.send(html)
-
-        })
-
-        app.post('/', function (req, res) {
-            try {
-                const { username, password } = req.body
-                userAuthenticator.authenticate(username, password)
-                const html = TemplateRenderer.createFromFile("home.html").renderWithoutData()
-                res.send(html)
-            } catch (err) {
-                if(err instanceof AuthenticationError){
-                    const data = {validation : "Login Failed"}
-                    const html = TemplateRenderer.createFromFile("index.html").renderWithData(data)
-                    res.status(200).send(html)
-                }else{
-                    const html = TemplateRenderer.createFromFile("error.html").renderWithoutData()
-                    res.status(200).send(html)
-                }
-                
-            }
-        })
-
+        app.post('/', this.loginHandler)
+        app.get('/', this.homeHandler)
+        app.post('/logout', this.logoutHandler)
         //static folders
-        const resolvedPath =  path.join(__dirname, '../../../public')
-        console.log(`resolved path: ${resolvedPath}`)
+        const resolvedPath = path.join(__dirname, '../../../public')
         app.use('/static', express.static(resolvedPath))
         app.listen(3000)
 
     }
+
+    loginHandler = (req, res) => {
+        try {
+            const { username, password } = req.body
+            const { token, session } = this.userAuthenticator.authenticate(username, password)
+            const { userAccount, expiresAt } = session
+            const html = TemplateRenderer.createFromFile("home.html").renderWithData(userAccount)
+            res.cookie("session_token", token, { expires: expiresAt })
+            res.send(html)
+        } catch (err) {
+            if (err instanceof AuthenticationError) {
+                const data = { validation: "Login Failed" }
+                const html = TemplateRenderer.createFromFile("login.html").renderWithData(data)
+                res.status(200).send(html)
+            } else {
+                this.errorHandler(err, res)
+            }
+        }
+    }
+
+    homeHandler = (req, res) => {
+        try {
+            const sessionToken = this.getSessionToken(req)
+            const session = this.userAuthenticator.validateSession(sessionToken)
+            let html
+            if (session) {
+                html = TemplateRenderer.createFromFile("home.html").renderWithData(session.userAccount)
+            }
+            else {
+                html = TemplateRenderer.createFromFile("login.html").renderWithoutData()
+            }
+            res.send(html)
+        } catch (err) {
+            this.errorHandler(err, res)
+        }
+    }
+
+    sessionHandler = (req, res, innerHandler) => {
+        const sessionToken = this.getSessionToken(req)
+        const session = this.userAuthenticator.validateSession(sessionToken)
+        if (!session) {
+            const html = TemplateRenderer.createFromFile("login.html").renderWithoutData()
+            res.send(html)
+        }else{
+            innerHandler(req, res)
+        }
+    }
+
+    getSessionToken = (req) => {
+        const cookies = req.cookies
+        if (cookies) {
+            const sessionToken = cookies["session_token"]
+            return sessionToken
+        } else {
+            return undefined
+        }
+    }
+
+
+
+
+
+    logoutHandler = (req, res) => {
+        try {
+            console.log("logout was triggered")
+            const sessionToken = this.getSessionToken(req)
+            this.userAuthenticator.endSession(sessionToken)
+            res.cookie("session_token", "", { expires: new Date() })
+            res.redirect("/")
+        } catch (err) {
+            this.errorHandler(err, res)
+        }
+
+    }
+
+    errorHandler = (error, res) => {
+        const html = TemplateRenderer.createFromFile("error.html").renderWithoutData()
+        console.log(`An error occurred, message: ${error.message}`)
+        console.error(error)
+        res.status(200).send(html)
+    }
+
 }
 
 module.exports = WebServer
